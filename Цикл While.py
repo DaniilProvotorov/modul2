6dba807cefbffcf199d14c5949022d76d4d49df5
DaniilProvotorov <dantcsar@gmail.com>
2024-10-20 22:51:25 +0500

    Цикл While.py

--- Цикл While.py
+++ Цикл While.py
@@ -1,28 +1,10 @@
-my_list =  [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-print("Длина списка:", len(my_list))
-while True:
-    if my_list[0] > 0:
-        print(my_list[0])
-    if my_list[1] > 0:
-        print(my_list[1])
-    if my_list[2] > 0:
-        print(my_list[2])
-    if my_list[3] > 0:
-        print(my_list[3])
-    if my_list[4] > 0:
-        print(my_list[4])
-    if my_list[5] > 0:
-        print(my_list[5])
-    if my_list[6] > 0:
-        print(my_list[6])
-    if my_list[7] > 0:
-        print(my_list[7])
-    if my_list[8] > 0:
-        print(my_list[8])
-    if my_list[9] > 0:
-        print(my_list[9])
-    if my_list[10] > 0:
-        print(my_list[10])
-    if my_list[11] > 0:
-        print(my_list[11])
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+number = 0
+while number <= len(my_list):
+    if number >= len(my_list):
         break
+    if my_list[number] > 0:
+        print(my_list[number])
+        number = number + 1
+    elif my_list[number] <= 0:
+        number = number + 1
